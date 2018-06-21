3c96a2beba2c1d4d8273a9b488719f5fa46b43b5
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-21 06:52:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,6 @@ def line(katz_deli)
   else
     a = katz_deli.map.with_index(1) do |name, index|
       a << "#{index}. #{name}"
-      index += 1
     end
     puts "The line is currently: #{a.join(" ")}"
   end
