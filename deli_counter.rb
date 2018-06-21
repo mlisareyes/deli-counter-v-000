3cdb810139cc695cf84b4f6c54177317742c77af
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-21 07:08:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "There is nobody waiting to be served!"
-end
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{name}"
