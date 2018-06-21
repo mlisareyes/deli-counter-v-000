256b8b2cd284cf28e83b807a5d892ded5de8f1cc
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-21 06:49:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.map.with_index(1) do |name, index|
+    a = katz_deli.map.with_index(1) do |name, index|
       a << "#{index}. #{name}"
       index += 1
-
-
     end
     puts "The line is currently: a.join(" ")"
   end
